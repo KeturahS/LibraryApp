2f89f0ca1361d01c5840cbbd69661e23d0abd669
amuev213 <amuev213@gmail.com>
2024-12-23 12:46:10 +0200

    adding book model and book controller(empty)

---
+++ LibraryApp/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryApp.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public decimal BorrowPrice { get; set; }
+        public decimal BuyPrice { get; set; }
+        public int AvailableCopies { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
